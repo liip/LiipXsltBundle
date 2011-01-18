0493d28937cf6f8d6f3092b18183709d4550c68d
Jordi Boggiano <j.boggiano@seld.be>
2011-01-18 17:38:51 +0800

    Updated the bundle for the latest Symfony2 version

--- XsltEngine.php
+++ XsltEngine.php
@@ -2,22 +2,28 @@
 
 namespace Bundle\Liip\XsltBundle;
 
-use Symfony\Component\Templating\Renderer\Renderer as BaseRenderer;
+use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Router;
-use Symfony\Component\Templating\Storage\Storage;
+use Symfony\Component\Templating\Loader\LoaderInterface;
 use Symfony\Component\Templating\Storage\FileStorage;
 
-class Renderer extends BaseRenderer
+class XsltEngine implements EngineInterface
 {
+    protected $container;
+    protected $loader;
     protected $kernel;
     protected $request;
     protected $router;
     protected $options;
 
-    public function __construct(Kernel $kernel, Request $request, Router $router, $options = array())
+    public function __construct(ContainerInterface $container, LoaderInterface $loader, Kernel $kernel, Request $request, Router $router, $options = array())
     {
+        $this->container = $container;
+        $this->loader = $loader;
         $this->kernel = $kernel;
         $this->request = $request;
         $this->options = $options;
@@ -25,16 +31,21 @@ class Renderer extends BaseRenderer
     }
 
     /**
-     * Evaluates a template.
+     * Renders a template.
      *
-     * @param Storage $template   The template to render
-     * @param array   $parameters An array of parameters to pass to the template
+     * @param string $name       A template name
+     * @param array  $parameters An array of parameters to pass to the template
      *
-     * @return string|false The evaluated template, or false if the renderer is unable to render the template
+     * @return string The evaluated template as a string
+     *
+     * @throws \InvalidArgumentException if the template does not exist
+     * @throws \RuntimeException         if the template cannot be rendered
      */
-    public function evaluate(Storage $template, array $parameters = array())
+    public function render($name, array $parameters = array())
     {
         $dom = new \DOMDocument();
+
+        $template = $this->load($name);
 
         if ($template instanceof FileStorage) {
             $dom->load($template);
@@ -135,5 +146,74 @@ class Renderer extends BaseRenderer
         }
 
         return $xsl->transformToXML($dom);
+    }
+
+    /**
+     * Returns true if the template exists.
+     *
+     * @param string $name A template name
+     *
+     * @return Boolean true if the template exists, false otherwise
+     */
+    public function exists($name)
+    {
+        try {
+            $this->load($name);
+        } catch (\Twig_Error_Loader $e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Loads the given template.
+     *
+     * @param string $name A template name
+     *
+     * @return Storage A Storage instance
+     *
+     * @throws \InvalidArgumentException if the template cannot be found
+     */
+    public function load($name)
+    {
+        $template = $this->loader->load($name);
+        if (false === $template) {
+            throw new \InvalidArgumentException(sprintf('The template "%s" does not exist.', $name));
+        }
+
+        return $template;
+    }
+
+    /**
+     * Returns true if this class is able to render the given template.
+     *
+     * @param string $name A template name
+     *
+     * @return boolean True if this class supports the given resource, false otherwise
+     */
+    public function supports($name)
+    {
+        return false !== strpos($name, '.xsl');
+    }
+
+    /**
+     * Renders a view and returns a Response.
+     *
+     * @param string   $view       The view name
+     * @param array    $parameters An array of parameters to pass to the view
+     * @param Response $response   A Response instance
+     *
+     * @return Response A Response instance
+     */
+    public function renderResponse($view, array $parameters = array(), Response $response = null)
+    {
+        if (null === $response) {
+            $response = $this->container->get('response');
+        }
+
+        $response->setContent($this->render($view, $parameters));
+
+        return $response;
     }
 }
